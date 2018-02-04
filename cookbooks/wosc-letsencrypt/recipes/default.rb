7abbde1d3e78a711eb599233558c1ebb8eb2ea24
Wolfgang Schnerring <wosc@wosc.de>
2018-02-04 18:44:33 +0100

    Deploy letsencrypt simp-le client

---
+++ cookbooks/wosc-letsencrypt/recipes/default.rb
@@ -0,0 +1,70 @@
+wosc_service_user "letsencrypt" do
+  shell "/bin/bash"
+end
+
+group "letsencrypt" do
+  action :manage
+  append true
+  members "www-data"
+  only_if "id -a www-data"
+end
+
+group "letsencrypt" do
+  action :manage
+  append true
+  members "Debian-exim"
+  only_if "id -a Debian-exim"
+end
+
+include_recipe "wosc-letsencrypt::items"
+
+
+# Install simp_le client <https://github.com/zenhack/simp_le>
+
+python_runtime "letsencrypt" do
+  version "3.5"
+  provider :system
+end
+
+python_virtualenv "/srv/letsencrypt/deployment" do
+  user "letsencrypt"
+  group "letsencrypt"
+  python "letsencrypt"
+  setuptools_version "38.4.1"
+  pip_version "9.0.1"
+  wheel_version "0.30.0"
+end
+
+template "/srv/letsencrypt/requirements.txt" do
+  source "requirements.txt"
+end
+
+pip_requirements "/srv/letsencrypt/requirements.txt" do
+  virtualenv "/srv/letsencrypt/deployment"
+  options "--no-deps"
+end
+
+
+# nginx integration
+
+template "/etc/nginx/snippets/letsencrypt.conf" do
+  source "nginx.conf"
+end
+
+template "/etc/sudoers.d/letsencrypt" do
+  source "sudo.conf"
+end
+template "/srv/letsencrypt/update-letsencrypt" do
+  source "update-letsencrypt"
+  mode "0755"
+  owner "letsencrypt"
+  group "letsencrypt"
+end
+
+cron "update-letsencrypt" do
+  command "/srv/letsencrypt/update-letsencrypt"
+  hour "2"
+  minute "15"
+  user "letsencrypt"
+  mailto "wosc@wosc.de"
+end
---
+++ cookbooks/wosc-letsencrypt/recipes/items.rb
@@ -0,0 +1,40 @@
+directory "/srv/letsencrypt/public" do
+  owner "letsencrypt"
+end
+directory "/srv/letsencrypt/data" do
+  owner "letsencrypt"
+  group "letsencrypt"
+  mode "0770"
+end
+
+
+[
+  "grmusik.de",
+  "mail.wosc.de",
+  "pharos.wosc.de",
+].each do |domain|
+  directory "/srv/letsencrypt/public/#{domain}" do
+    owner "letsencrypt"
+  end
+
+  directory "/srv/letsencrypt/data/#{domain}" do
+    owner "letsencrypt"
+  end
+  cookbook_file "/srv/letsencrypt/data/#{domain}/account_key.json" do
+    source "#{domain}.account"
+    owner "letsencrypt"
+    group "letsencrypt"
+    mode "0600"
+  end
+  cookbook_file "/srv/letsencrypt/data/#{domain}/key.pem" do
+    source "#{domain}.key"
+    owner "letsencrypt"
+    group "letsencrypt"
+    mode "0640"
+  end
+end
+
+cookbook_file "/srv/letsencrypt/data/mail.wosc.de/update" do
+  source "mail.wosc.de.update"
+  mode "0755"
+end
