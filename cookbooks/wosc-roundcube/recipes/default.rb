b65444fb555191d203e494a79ce1016a9c5a5596
Wolfgang Schnerring <wosc@wosc.de>
2018-03-03 15:15:48 +0100

    Deploy roundcube webmail application

---
+++ cookbooks/wosc-roundcube/recipes/default.rb
@@ -0,0 +1,72 @@
+include_recipe "wosc-fastcgi::php"
+package "php7.0-intl"
+package "php7.0-json"
+package "php7.0-mbstring"
+package "php7.0-mcrypt"
+package "php7.0-mysql"
+package "php7.0-xml"
+
+wosc_service_user "roundcube" do
+  shell "/bin/bash"
+end
+
+VERSION = '1.3.4'
+
+ark "roundcube" do
+  url "https://github.com/roundcube/roundcubemail/releases/download/#{VERSION}/roundcubemail-#{VERSION}-complete.tar.gz"
+  action :put
+  path "/srv"
+  owner "roundcube"
+  group "roundcube"
+end
+
+directory "/srv/roundcube/installer" do
+  action :delete
+  recursive true
+end
+
+template "/srv/roundcube/config/config.inc.php" do
+  source "config.php"
+  owner "roundcube"
+  group "roundcube"
+  mode "0640"
+end
+
+wosc_mysql_database "roundcube"
+wosc_mysql_user "roundcube" do
+  password node["roundcube"]["db_pass"]
+end
+wosc_mysql_grant "roundcube" do
+  user "roundcube"
+end
+execute "roundcube_db_schema" do
+  command "mysql -Bs -uroot roundcube < /srv/roundcube/SQL/mysql.initial.sql"
+  not_if "echo 'show tables' | mysql -uroot roundcube | grep -q ."
+end
+
+
+include_recipe "wosc-fastcgi::supervisor"
+template "/etc/supervisor/conf.d/roundcube.conf" do
+  source "supervisor.conf"
+  # XXX Too agressive since it restarts *all* services
+  # notifies :reload, "service[supervisor]", :delayed
+end
+
+
+include_recipe "wosc-fastcgi::nginx"
+template "/etc/nginx/sites-available/mail.wosc.de" do
+  source "nginx.conf"
+  notifies :reload, "service[nginx]", :delayed
+end
+link "/etc/nginx/sites-enabled/mail.wosc.de" do
+  to "/etc/nginx/sites-available/mail.wosc.de"
+  notifies :reload, "service[nginx]", :delayed
+end
+
+
+package "imapproxy"
+service "imapproxy"
+execute "sed -i -e 's/#listen_address 127.0.0.1/listen_address 127.0.0.1/' /etc/imapproxy.conf" do
+  only_if "grep -q '#listen_address' /etc/imapproxy.conf"
+  notifies :restart, "service[imapproxy]", :delayed
+end
---
+++ cookbooks/wosc-roundcube/templates/default/config.php
@@ -0,0 +1,26 @@
+<?php
+$config = array();
+
+$config['db_dsnw'] = 'mysql://roundcube:<%=node["roundcube"]["db_pass"]%>@localhost/roundcube';
+
+$config['default_host'] = 'localhost';
+$config['default_port'] = 1143;
+$config['smtp_server'] = 'localhost';
+$config['smtp_port'] = 25;
+
+$config['product_name'] = 'Roundcube Webmail';
+$config['support_url'] = '';
+
+// this key is used to encrypt the users imap password which is stored
+// in the session record (and the client cookie if remember password is enabled).
+// please provide a string of exactly 24 chars.
+$config['des_key'] = '<%=node["roundcube"]["store_pass_key"]%>';
+
+// List of active plugins (in plugins/ directory)
+$config['plugins'] = array(
+    'archive',
+    'zipdownload',
+);
+
+// skin name: folder from skins/
+$config['skin'] = 'larry';
