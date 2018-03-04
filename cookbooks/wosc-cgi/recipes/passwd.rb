94fd24696deece12fd21e59f42ccd7388c1196ae
Wolfgang Schnerring <wosc@wosc.de>
2018-03-04 17:04:31 +0100

    Deploy webpasswd application

---
+++ cookbooks/wosc-cgi/recipes/passwd.rb
@@ -0,0 +1,37 @@
+python_runtime "passwd" do
+  version "3.5"
+  provider :system
+end
+
+python_virtualenv "/srv/cgiserv/passwd" do
+  user "cgiserv"
+  group "cgiserv"
+  python "passwd"
+  setuptools_version "38.4.1"
+  pip_version "9.0.1"
+  wheel_version "0.30.0"
+end
+
+template "/srv/cgiserv/passwd/requirements.txt" do
+  source "passwd/requirements.txt"
+end
+
+pip_requirements "/srv/cgiserv/passwd/requirements.txt" do
+  virtualenv "/srv/cgiserv/passwd"
+  options "--no-deps"
+end
+
+
+file "/etc/sudoers.d/webpasswd" do
+  content "cgiserv ALL=(root) NOPASSWD: /srv/cgiserv/passwd/bin/webpasswd-change\n"
+end
+
+
+file "/srv/cgiserv/apache.d/passwd.conf" do
+  content "ScriptAlias /passwd /srv/cgiserv/passwd/bin/webpasswd-cgi\n"
+  notifies :run, "execute[supervisorctl restart cgiserv]", :delayed
+end
+file "/srv/cgiserv/nginx.d/passwd.conf" do
+  content "location /passwd { proxy_pass http://cgi; }\n"
+  notifies :reload, "service[nginx]", :delayed
+end
