de13792195d8f4d1040f14992b75cb260da989bf
Wolfgang Schnerring <wosc@wosc.de>
2018-03-11 12:02:07 +0100

    Fix radicale courier error handling bug

--- cookbooks/wosc-radicale/recipes/default.rb
+++ cookbooks/wosc-radicale/recipes/default.rb
@@ -33,7 +33,7 @@ pip_requirements "/srv/radicale/requirements.txt" do
   options "--no-deps"
 end
 
-execute "sed -i -e 's/sock.send(line)/sock.send(line.encode(\"utf-8\"))/' -e 's/\"GID\"/b\"GID\"/' /srv/radicale/deployment/lib/python3.5/site-packages/radicale/auth/courier.py" do
+execute "sed -i -e 's/sock.send(line)/sock.send(line.encode(\"utf-8\"))/' -e 's/\"GID\"/b\"GID\"/' -e 's/except Exception:/except Exception as exception/' /srv/radicale/deployment/lib/python3.5/site-packages/radicale/auth/courier.py" do
   not_if "grep -q 'b\"GID\"' /srv/radicale/deployment/lib/python3.5/site-packages/radicale/auth/courier.py"
 end
 
