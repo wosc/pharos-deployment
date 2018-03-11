f15042108bd1b9aee79dc79e8023ddb4932d9ee4
Wolfgang Schnerring <wosc@wosc.de>
2018-03-11 12:02:25 +0100

    Deploy agendav calendar UI
    It doesn't work yet, because it requests
    http://calendar.wosc.de/test@example.com/ even though
    http://calendar.wosc.de/cal/ is configured as the base url.

---
+++ cookbooks/wosc-agendav/recipes/default.rb
@@ -0,0 +1,64 @@
+include_recipe "wosc-fastcgi::php"
+package "php7.0-ctype"
+package "php7.0-curl"
+package "php7.0-mbstring"
+package "php7.0-mcrypt"
+package "php7.0-mysql"
+package "php7.0-tokenizer"
+package "php7.0-xml"
+package "php7.0-xmlreader"
+package "php7.0-xmlwriter"
+
+wosc_service_user "agendav" do
+  shell "/bin/bash"
+end
+
+VERSION = '2.2.0'
+
+ark "agendav" do
+  url "https://github.com/agendav/agendav/releases/download/#{VERSION}/agendav-#{VERSION}.tar.gz"
+  action :put
+  path "/srv"
+  owner "agendav"
+  group "agendav"
+end
+
+template "/srv/agendav/web/config/settings.php" do
+  source "settings.php"
+  owner "agendav"
+  group "agendav"
+  mode "0640"
+end
+
+wosc_mysql_database "agendav"
+wosc_mysql_user "agendav" do
+  password node["agendav"]["db_pass"]
+end
+wosc_mysql_grant "agendav" do
+  user "agendav"
+end
+
+execute "agendav_db_schema" do
+  command "php agendavcli migrations:migrate --no-interaction"
+  cwd "/srv/agendav"
+  not_if "echo 'show tables' | mysql -uroot agendav | grep -q ."
+end
+
+
+include_recipe "wosc-fastcgi::supervisor"
+template "/etc/supervisor/conf.d/agendav.conf" do
+  source "supervisor.conf"
+  # XXX Too agressive since it restarts *all* services
+  # notifies :reload, "service[supervisor]", :delayed
+end
+
+
+include_recipe "wosc-fastcgi::nginx"
+template "/etc/nginx/sites-available/calendar2.wosc.de" do
+  source "nginx.conf"
+  notifies :reload, "service[nginx]", :delayed
+end
+link "/etc/nginx/sites-enabled/calendar2.wosc.de" do
+  to "/etc/nginx/sites-available/calendar2.wosc.de"
+  notifies :reload, "service[nginx]", :delayed
+end
---
+++ cookbooks/wosc-agendav/templates/default/settings.php
@@ -0,0 +1,21 @@
+<?php
+
+$app['db.options'] = [
+        'dbname' => 'agendav',
+        'user' => 'agendav',
+        'password' => '<%=node["agendav"]["db_pass"]%>',
+        'host' => 'localhost',
+        'driver' => 'pdo_mysql'
+];
+
+$app['caldav.baseurl'] = 'http://calendar.wosc.de/cal/';
+$app['caldav.publicurls'] = false;
+// $app['caldav.baseurl.public'] = 'https://caldav.server.com';
+
+$app['site.title'] = 'Calendar';
+$app['csrf.secret'] = '<%=node["agendav"]["csrf_secret"]%>';
+$app['proxies'] = ['127.0.0.1'];
+$app['defaults.timezone'] = 'Europe/Berlin';
+$app['defaults.show_week_nb'] = true;
+
+$app['log.level'] = 'INFO';
