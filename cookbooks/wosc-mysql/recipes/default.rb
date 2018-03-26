aa29239567f55e67ee0d06dec0edd916e8fbdba9
Wolfgang Schnerring <wosc@wosc.de>
2018-03-26 14:22:50 +0200

    Deploy daily mysql backup cronjob

--- cookbooks/wosc-mysql/recipes/default.rb
+++ cookbooks/wosc-mysql/recipes/default.rb
@@ -1,3 +1,14 @@
 package "mysql-server-5.7"
 package "mysql-client-5.7"
 
+template "/usr/local/bin/mysql-backup" do
+  source "mysql-backup"
+  mode "0755"
+end
+template "/usr/local/bin/mysql-restore" do
+  source "mysql-restore"
+  mode "0755"
+end
+link "/etc/cron.daily/mysql-backup" do
+  to "/usr/local/bin/mysql-backup"
+end
