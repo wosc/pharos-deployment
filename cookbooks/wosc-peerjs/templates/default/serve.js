d25eab356b6881db263e00d5589cdb4f454c4d07
Wolfgang Schnerring <wosc@wosc.de>
2018-06-30 10:35:08 +0200

    Listen on localhost, public-facing things are proxied through nginx

--- cookbooks/wosc-peerjs/templates/default/serve.js
+++ cookbooks/wosc-peerjs/templates/default/serve.js
@@ -1,7 +1,38 @@
+var express = require('express');
+var http = require('http');
 var pkg = require('./package.json');
-var PeerServer = require('./lib').PeerServer;
+var ExpressPeerServer = require('./lib').ExpressPeerServer;
+
+
+// copy&paste to add listen host, removed ssl stuff
+function PeerServer(options, callback) {
+  options = options || {};
+  var path = options.path || '/';
+  var port = options.port || 80;
+  var host = options.host || '0.0.0.0';
+  delete options.path;
+
+  if (path[0] !== '/') {
+    path = '/' + path;
+  }
+  if (path[path.length - 1] !== '/') {
+    path += '/';
+  }
+
+  var app = express();
+  var server = http.createServer(app);
+  var peerjs = ExpressPeerServer(server, options);
+  app.use(path, peerjs);
+
+  server.listen(port, host, function() {
+    callback(server);
+  });
+
+  return peerjs;
+};
+
 var server = PeerServer(
-    {port: 7077, path: '/', proxied: true}, function(server) {
+    {host: '127.0.0.1', port: 7077, path: '/', proxied: true}, function(server) {
   var host = server.address().address;
   var port = server.address().port;
 
