577ed98059c57a2b4cdff731bcdd0dedef109e38
Wolfgang Schnerring <wosc@wosc.de>
2019-08-10 15:51:48 +0200

    Deploy RSS bridge

---
+++ cookbooks/wosc-rssbridge/recipes/default.rb
@@ -0,0 +1,40 @@
+include_recipe "wosc-fastcgi::php"
+package "php7.2-curl"
+package "php7.2-json"
+package "php7.2-mbstring"
+package "php7.2-xml"
+
+
+wosc_service_user "rssbridge"
+
+VERSION = '2019-07-06'
+
+directory "/srv/rssbridge/public" do
+  owner "rssbridge"
+end
+
+ark "rssbridge" do
+  url "https://github.com/RSS-Bridge/rss-bridge/archive/#{VERSION}.tar.gz"
+  action :put
+  path "/srv/rssbridge"
+  name "public"
+  owner "rssbridge"
+  group "rssbridge"
+end
+
+file "/srv/rssbridge/public/whitelist.txt" do
+  content "*"
+end
+
+
+include_recipe "wosc-fastcgi::supervisor"
+template "/etc/supervisor/conf.d/rssbridge.conf" do
+  source "supervisor.conf"
+  notifies :run, "execute[reload_supervisor]", :delayed
+end
+
+include_recipe "wosc-fastcgi::nginx"
+template "/srv/rssbridge/nginx.conf" do
+  source "nginx.conf"
+  notifies :reload, "service[nginx]", :delayed
+end
