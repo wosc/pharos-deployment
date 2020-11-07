1b3764624c3a8f3a59906037d3251eacd2b7c3cf
Wolfgang Schnerring <wosc@wosc.de>
2020-11-07 15:56:06 +0100

    Deploy alternative (complete :) roborally board game

--- cookbooks/wosc-peerjs/attributes/default.rb
+++ cookbooks/wosc-peerjs/attributes/default.rb
@@ -1,4 +1,4 @@
 default["nodejs"]["install_method"] = "binary"
-default["nodejs"]["version"] = "8.17.0"
+default["nodejs"]["version"] = "12.19.0"
 default["nodejs"]["binary"]["checksum"] = (
-  "8b2c9e1f84317c4b02736c4c50db4dd2cd6c4f0ba910fa81f887c8c9294af596")
+  "f37a5bf0965e8ab7b1b078392638778286ceee8fdb895c050889a61772944bda")
