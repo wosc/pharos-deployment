3dfa42ccf59377be13c1a0941ce9322bd066f22e
Wolfgang Schnerring <wosc@wosc.de>
2020-12-22 11:47:40 +0100

    Migrate wosc::cron-apt

---
+++ batou_ext/src/batou_ext/file.py
@@ -0,0 +1,20 @@
+from batou import UpdateNeeded
+from batou.component import Component
+import batou.lib.file
+import os.path
+
+
+class Delete(Component):
+    """XXX batou.lib.File really should support `ensure=absent`."""
+
+    namevar = 'path'
+
+    def configure(self):
+        self.path = self.map(self.path)
+
+    def verify(self):
+        if os.path.isfile(self.path):
+            raise UpdateNeeded()
+
+    def update(self):
+        batou.lib.file.ensure_path_nonexistent(self.path)
--- components/base/component.py
+++ components/base/component.py
@@ -1,5 +1,7 @@
 from batou.component import Component
+from batou.lib.file import File
 from batou_ext.apt import Package
+from batou_ext.file import Delete
 
 
 class BasePackages(Component):
@@ -21,3 +23,12 @@ class BasePackages(Component):
     def configure(self):
         for name in self.packages:
             self += Package(name)
+
+
+class CronAPT(Component):
+
+    def configure(self):
+        Delete('/etc/apt/apt.conf.d/20auto-upgrades')
+        self += Package('cron-apt')
+        self += File(
+            '/etc/cron-apt/config', source='cron-apt.conf', is_template=False)
