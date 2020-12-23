757c60f0756dfea63cc20c8074849cefc7362f1b
Wolfgang Schnerring <wosc@wosc.de>
2020-12-23 10:14:08 +0100

    Implement creating users and groups

--- batou_ext/src/batou_ext/user.py
+++ batou_ext/src/batou_ext/user.py
@@ -1,5 +1,42 @@
 from batou import UpdateNeeded
-from batou.component import Component
+from batou.component import Component, Attribute
+import grp
+import pwd
+
+
+class User(Component):
+
+    namevar = 'user'
+    shell = '/bin/bash'
+    home = Attribute(default='/srv/{{component.user}}')
+
+    def verify(self):
+        try:
+            pwd.getpwnam(self.user)
+        except KeyError:
+            raise UpdateNeeded()
+
+    def update(self):
+        self.cmd(self.expand(
+            'adduser '
+            '--home {{component.home}} '
+            '--shell {{component.shell}} '
+            '{{component.user}}'
+        ))
+
+
+class Group(Component):
+
+    namevar = 'group'
+
+    def verify(self):
+        try:
+            grp.getgrnam(self.group)
+        except KeyError:
+            raise UpdateNeeded()
+
+    def update(self):
+        self.cmd('addgroup %s' % self.group)
 
 
 class GroupMember(Component):
