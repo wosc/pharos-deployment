29ad840a7d276fcb217b4b8d19d130a3aaf96b2a
Wolfgang Schnerring <wosc@wosc.de>
2020-12-23 11:27:37 +0100

    Migrate wosc-cgiserv::passwd

--- components/cgi/component.py
+++ components/cgi/component.py
@@ -1,5 +1,6 @@
 from batou.component import Component
 from batou.lib.file import File
+from batou_ext.nginx import VHost
 from batou_ext.python import VirtualEnv, Requirements
 from batou_ext.apache import CGI
 
@@ -22,3 +23,24 @@ class UptimeRobot(Component):
         self += File('/srv/cgiserv/apache.d/uptimerobot.conf',
                      source='uptimerobot/apache.conf', is_template=False)
         self += CGI(self._)
+
+
+class WebPasswd(Component):
+
+    def configure(self):
+        self += VirtualEnv(path='/srv/cgiserv/passwd')
+        self._ += Requirements(source='passwd.txt')
+
+        self += File(
+            '/etc/sudoers.d/webpasswd',
+            content='cgiserv ALL=(root) NOPASSWD: /srv/cgiserv/passwd/bin/webpasswd-change\n')
+
+        self += File(
+            '/srv/cgiserv/apache.d/passwd.conf',
+            content='ScriptAlias /passwd /srv/cgiserv/passwd/bin/webpasswd-cgi\n' )
+        self += CGI(self._)
+
+        self += File(
+            '/srv/cgiserv/nginx.d/passwd.conf',
+            content='location /passwd { proxy_pass http://cgi; }\n')
+        self += VHost(self._)
