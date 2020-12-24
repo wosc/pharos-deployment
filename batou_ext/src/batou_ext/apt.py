765ee51a789c29fbb9af17849206f3d8567d5728
Wolfgang Schnerring <wosc@wosc.de>
2020-12-24 11:49:54 +0100

    Implement adding an apt repository source and gpg key

--- batou_ext/src/batou_ext/apt.py
+++ batou_ext/src/batou_ext/apt.py
@@ -1,5 +1,9 @@
 from batou import UpdateNeeded
 from batou.component import Component
+from batou.lib.file import File
+import os
+import re
+import requests
 
 
 class Package(Component):
@@ -28,3 +32,70 @@ class Package(Component):
         self.cmd('apt -y %s install %s%s' % (
             self.options, self.name, version),
             env={'DEBIAN_FRONTEND': 'noninteractive'})
+
+
+class AptRepository(Component):
+
+    namevar = 'name'
+    line = None
+    key = None
+
+    def configure(self):
+        self += AptKey(self.key, name=self.name)
+        self += File('/etc/apt/sources.list.d/%s.list' % self.name,
+                     content=self.line)
+        self.config = self._
+
+    def verify(self):
+        self.config.assert_no_changes()
+
+    def update(self):
+        self.cmd('apt update')
+
+
+class AptKey(Component):
+
+    namevar = 'url'
+    name = None
+
+    def configure(self):
+        self.key = self.map(self.name) + '.pub'
+        self += Download(self.url, target=self.key)
+
+    def verify(self):
+        # Adapted from <https://github.com/chef-cookbooks/apt/blob/v5.0.0
+        #   /providers/repository.rb#L112-L121>
+        installed = self.fingerprints(self.cmd('apt-key finger')[0])
+        new = self.fingerprints(self.cmd(
+            'gpg --with-subkey-fingerprint %s' % self.key)[0])
+        if new - installed:
+            raise UpdateNeeded()
+
+    def update(self):
+        self.cmd('apt-key add %s' % self.key)
+
+    KEY = re.compile('^ +([0-9A-F ]+)$')  # scary gpg output parsing
+
+    def fingerprints(self, text):
+        result = set()
+        for line in text.splitlines():
+            match = self.KEY.search(line)
+            if match:
+                result.add(match.group(1).replace(' ', ''))
+        return result
+
+
+class Download(Component):
+
+    namevar = 'url'
+    target = None
+
+    def verify(self):
+        if not os.path.exists(self.target):
+            raise UpdateNeeded()
+
+    def update(self):
+        r = requests.get(self.url)
+        r.raise_for_status()
+        with open(self.target, 'w') as f:
+            f.write(r.text)
