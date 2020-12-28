1e7a4e030e540351a806602a987666b582ec88b7
Wolfgang Schnerring <wosc@wosc.de>
2020-12-28 10:37:56 +0100

    Migrate wosc-robometeor

--- components/games/component.py
+++ components/games/component.py
@@ -1,5 +1,6 @@
 from batou.component import Component
 from batou.lib.file import File
+from batou_ext.apt import Package
 from batou_ext.nginx import VHost
 from batou_ext.python import VirtualEnv, Requirements
 from batou_ext.supervisor import Program
@@ -22,4 +23,36 @@ class Tabu(Component):
 
         self += File(
             '/srv/tabu/nginx.conf', source='tabu.conf', is_template=False)
+        self += VHost(self._)
+
+
+class Roborally(Component):
+
+    def configure(self):
+        self += Package('mongodb')
+        self += User('robometeor')
+
+        # meteor build dist
+        # scp dist/robometeor.tar.gz wosc.de:/srv/robometeor
+        # tar xf /srv/robometeor/robometeor.tar.gz
+        # cd bundle/programs/server && npm install
+
+        # replace in programs/web.browser/12345.js:
+        # `Router.route('/` with `Router.router('/roborally/`
+        # prefix with `/roborally`:
+        # `/robots/`, `/tiles/`, `/finish.png`, `/start.png`,
+        # `/damage-token.png`, `/Power_Off.png`
+
+        self += Program(
+            'robometeor',
+            command='node /srv/robometeor/bundle/main.js',
+            environ='BIND_IP=127.0.0.1, PORT=7082, '
+            'ROOT_URL=https://wosc.de/roborally, '
+            'MONGO_URL=mongodb://localhost:27017/robometeor',
+            user='robometeor',
+            dependencies=())
+
+        self += File(
+            '/srv/robometeor/nginx.conf', source='roborally.conf',
+            is_template=False)
         self += VHost(self._)
