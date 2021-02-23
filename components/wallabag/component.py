f96052a1d2a1bfb65db308b07f9ffd9827ad2e24
Wolfgang Schnerring <wosc@wosc.de>
2021-02-23 20:09:43 +0100

    Add missing dependency

--- components/wallabag/component.py
+++ components/wallabag/component.py
@@ -23,6 +23,7 @@ class Wallabag(Component):
         'php7.4-bcmath',
         'php7.4-curl',
         'php7.4-gd',
+        'php7.4-intl',
         'php7.4-mbstring',
         'php7.4-mysql',
         'php7.4-tidy',
